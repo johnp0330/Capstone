57474ad9ca657ffab3707250a429737549b2c13a
Karter Phillips <62188085+KarterJP@users.noreply.github.com>
2020-10-25 20:24:56 -0400

    Images fixed
    Images will now be served to the webpage on startup

--- nuts & bolts/src/main/java/org/example/Application.java
+++ nuts & bolts/src/main/java/org/example/Application.java
@@ -1,14 +1,12 @@
 package org.example;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
+import javax.servlet.http.HttpSession;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.io.StringWriter;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.io.*;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -41,11 +39,16 @@ public class Application extends AbstractHandler
             {
                 page.append(line);
             }
-            // Adding the css to the page
+
+            // Adding the css and js to the page
             page.append("<head> " +
                         "<style>");
             page.append(loadResource("/style.css"));
             page.append("</style>");
+            page.append("<script>");
+            page.append(loadResource("/script.js"));
+            page.append("</script>");
+
             // Continuing to read the rest of the page
             while ((line = reader.readLine()) != null) {
                 page.append(line);
@@ -103,6 +106,27 @@ public class Application extends AbstractHandler
         else if (uri.contains(".html"))
         {
             page = loadPage(uri);
+        }
+        else if (uri.contains(".png") || uri.contains(".jpg"))
+        {
+            String baseUrl = "src/main/resources";
+
+            String mime = uri.contains(".png") ? "image/png" : "image/jpeg";
+
+            response.setContentType(mime);
+            File file = new File(baseUrl + uri);
+            response.setContentLength((int)file.length());
+
+            FileInputStream in = new FileInputStream(file);
+            OutputStream out = response.getOutputStream();
+
+            byte[] buf = new byte[1024];
+            int count = 0;
+            while ((count = in.read(buf)) >= 0) {
+                out.write(buf, 0, count);
+            }
+            in.close();
+            out.close();
         }
 
         response.getWriter().println(page);
